b19be64e141d2aea497dbc8825c22dce3099a072
Andi Bambeck <andi@bambeck.co.uk>
2024-01-17 22:59:52 +0000

    Styling tweak

--- src/components/MuseumHighlight.vue
+++ src/components/MuseumHighlight.vue
@@ -2,21 +2,18 @@
   <div :class="['museum-highlight', 'is-' + theme, highlight.isPartner ? 'is-partner' : '']">
     <div :class="['museum-highlight__image', loading ? 'is-loading' : '']">
       <i class="museum-highlight__image-loading fa-solid fa-compass fa-spin fa-2xl"></i>
-      <img
-        v-if="typeof compImage === 'string'"
-        :src="compImage"
-        :alt="`Image of ${highlight.name}`"
-      />
-
-      <button
-        type="button"
-        class="museum-highlight__refresh-image"
-        @click="fetchImage()"
-        :disabled="loading"
-        aria-label="Refresh Image"
-      >
-        <i class="fa-solid fa-arrows-rotate"></i>
-      </button>
+      <template v-if="typeof compImage === 'string'">
+        <img :src="compImage" :alt="`Image of ${highlight.name}`" />
+        <button
+          type="button"
+          class="museum-highlight__refresh-image"
+          @click="fetchImage()"
+          :disabled="loading"
+          aria-label="Refresh Image"
+        >
+          <i class="fa-solid fa-arrows-rotate"></i>
+        </button>
+      </template>
     </div>
     <div class="museum-highlight__badge">
       <slot name="badge"></slot>
